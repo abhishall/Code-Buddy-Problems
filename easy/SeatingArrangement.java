094cd9e3c92cf11a39e12f7e037c1ccba15eeced
akoes <abhishekkansal97@gmail.com>
2018-09-29 23:38:31 +0530

    first program

---
+++ easy/SeatingArrangement.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.Scanner;
+
+public class SeatingArrangement {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int T = scn.nextInt();
+		while (T-- > 0) {
+			int n = scn.nextInt();
+			int m = scn.nextInt();
+
+			int count = m;
+			int window = 1;
+			int nonWindow = 2 * n + 1;
+			while (count > 0) {
+				if (count == 0) {
+					break;
+				}
+				if (nonWindow <= m) {
+					System.out.print(nonWindow + " ");
+					nonWindow++;
+					count--;
+				}
+				if (m == 0) {
+					break;
+				}
+				if (window <= m) {
+					System.out.print(window + " ");
+					window++;
+					count--;
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
